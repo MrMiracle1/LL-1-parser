93e088d4e782da6823b1207143cef456ab72ee16
1243702053@qq.com <1243702053@qq.com>
2020-11-14 01:05:41 +0800

    predict

--- 语法分析器/parser.cpp
+++ 语法分析器/parser.cpp
@@ -1,5 +1,6 @@
 #include"parser.h"
 #include<iostream>
+#include<stack>
 void parser::table_derivation()
 {
 	g.remove_recursion();
@@ -30,6 +31,14 @@ void parser::table_derivation()
 		{
 			if(parser_table[i].find(t)== parser_table[i].end())
 				parser_table[i][t] = -1;
+		}
+	}
+	for (token i : g.non_terminators)
+	{
+		for (token j : g.terminators)
+		{
+			if (parser_table[i].find(j) == parser_table[i].end())
+				parser_table[i][j] = -2;
 		}
 	}
 }
@@ -63,7 +72,7 @@ void parser::output_parser_table()
 				cout << "synch	";
 				continue;
 			}
-			else if(temp==0)
+			else if(temp==-2)
 			{
 				cout << "		";
 				continue;
@@ -76,5 +85,79 @@ void parser::output_parser_table()
 			cout << "	";
 		}
 		cout << endl;
+	}
+}
+
+void parser::predict()
+{
+	stack<token> parser_stack;
+	pair<deque<token>, deque<token>> left_pattern{ {},{g.start_token} };
+	parser_stack.push("dollar");
+	parser_stack.push(g.start_token);
+	vector<token>::iterator iter = token_stream.begin();
+	for (;iter != token_stream.end();)
+	{
+		for (token t : left_pattern.first)
+		{
+			cout << t;
+		}
+		for (token t : left_pattern.second)
+		{
+			cout << t;
+		}
+		cout << "			";
+		vector<token>::iterator iter2 = iter;
+		for (;iter2 != token_stream.end();iter2++)
+		{
+			cout << *iter2;
+		}
+		cout << "			";
+		if (find(g.terminators.begin(), g.terminators.end(), parser_stack.top()) != g.terminators.end())//Õ»¶¥Îª·ÇÖÕ½á·û
+		{
+			if (*iter == parser_stack.top())
+			{
+				iter++;
+			}
+			else
+			{
+				cout << "error:" << parser_stack.top() << " expected"<<endl;
+			}
+			parser_stack.pop();
+			if (left_pattern.second.size())
+			{
+				left_pattern.first.push_back(left_pattern.second.front());
+				left_pattern.second.pop_front();
+			}
+		}
+		else//Õ»¶¥ÎªÖÕ½á·û
+		{
+			int p = parser_table[parser_stack.top()][*iter];
+			if (p == -2)
+			{
+				cout << "error:" << *iter << " unexpected" << endl;
+			}
+			else if (p == -1)
+			{
+				cout << "error:"<<parser_stack.top()<<"expected"<<endl;
+				parser_stack.pop();
+			}
+			else
+			{
+				parser_stack.pop();
+				left_pattern.second.pop_front();
+				cout << g.productions[p].left << "=";
+				for (string j : g.productions[p].right)
+					cout << j;
+				cout << endl;
+				if (g.productions[p].right.front()!= "0")
+				{
+					for (int i = g.productions[p].right.size() - 1;i >= 0;i--)
+					{
+						parser_stack.push(g.productions[p].right[i]);
+					}
+					left_pattern.second.insert(left_pattern.second.begin(), g.productions[p].right.begin(), g.productions[p].right.end());
+				}				
+			}
+		}
 	}
 }
--- 语法分析器/parser.h
+++ 语法分析器/parser.h
@@ -6,7 +6,9 @@ class parser
 private:
 	grammar g;
 	map<token, map<token, int>> parser_table;
+	vector<token> token_stream = { {"n"},{"+"},{"n"}, {"dollar"} };
 public:
 	void table_derivation();
 	void output_parser_table();
+	void predict();
 };
