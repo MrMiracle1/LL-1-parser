64f8615ebc0df8f06fe84911fca53856caee39dd
1243702053@qq.com <1243702053@qq.com>
2020-11-15 17:46:48 +0800

    提交

---
+++ LL(1)语法分析器-2018211393-王一丰/grammar.cpp
@@ -0,0 +1,308 @@
+#include"grammar.h"
+#include<iostream>
+#include<algorithm>
+#include<iomanip>
+#include"split.h"
+void grammar::remove_recursion()//消除左递归
+{
+	int t = 0;//productions_class的初始化
+	for (production i : productions)
+	{
+		productions_class[i.left].insert(t);
+		t++;
+	}
+
+	set<token> no_recursion;//已消左递归的非终结符
+	vector<token> new_non_terminators;//产生的新终结符
+	for (const auto& non_terminator : non_terminators)//遍历所有终结符
+	{
+		set<int> new_production_class = productions_class[non_terminator];//该终结符的所有新产生式
+		for (int i : productions_class[non_terminator])//遍历该终结符的所有产生式
+		{
+			token first_token = productions[i].right.front();//提取产生式第一个字符
+			if (no_recursion.find(first_token) != no_recursion.end())//判断是否已消除左递归
+			{
+				new_production_class.erase(i);//删除在目前非终结符下的产生式
+				productions[i].right.pop_front();//删除产生式第一个字符
+				for (int j : productions_class[first_token])//遍历该字符对应的所有产生式
+				{
+					new_production_class.insert(productions.size());//向目前终结符加入一个产生式
+					production new_production = productions[i];//一个新产生式复制原产生式
+					new_production.right.insert(new_production.right.begin(), productions[j].right.begin(), productions[j].right.end());//原第一个字符的产生式右端插入原生产式前端
+					productions.emplace_back(new_production);//加入该新产生式
+				}
+			}
+		}
+		productions_class[non_terminator] = new_production_class;//更新该非终结符的所有产生式
+		vector<int> left_recursion_productions;//含左递归的产生式集合
+		for (int i : productions_class[non_terminator])//遍历该非终结符的所有产生式
+		{
+			const token& first_token = productions[i].right.front();//提取产生式第一个字符
+			if (first_token == non_terminator)//第一个字符为该非终结符
+			{
+				new_production_class.erase(i);//去除该产生式在非终结符下的记录
+				left_recursion_productions.emplace_back(i);//将更改产生式放入左递归产生式集合
+			}
+		}
+		if (left_recursion_productions.size())//该非终结符有左递归
+		{
+			productions_class[non_terminator] = new_production_class;//更新该非终结符的所有产生式
+			token new_non_terminator=non_terminator + "\'";//定义新非终结符
+			new_non_terminators.emplace_back(new_non_terminator);//加入非终结符集合
+			for (int i : productions_class[non_terminator])//遍历该非终结符下所有产生式
+			{
+				productions[i].right.emplace_back(new_non_terminator);//产生式尾部加入新非终结符
+			}
+			productions_class[non_terminator] = new_production_class;//更新该非终结符的所有产生式
+			productions_class[new_non_terminator].insert(productions.size());//增加新非终结符下一个产生式
+			production new_production;//新产生式定义
+			new_production.left = new_non_terminator;//新产生式左侧为新非终结符
+			new_production.right = deque<token>{ "ε" };//新产生式右侧为空
+			productions.emplace_back(new_production);//加入该新产生式
+			/*productions.emplace_back(new_non_terminator, deque<token>{NULL});*/
+			for (int i : left_recursion_productions)//遍历所有左递归产生式
+			{
+				productions[i].left = new_non_terminator;//产生式左端为新非终结符
+				productions[i].right.pop_front();//右端首字符弹出
+				productions[i].right.emplace_back(new_non_terminator);//右端末尾加入新非终结符 
+				productions_class[new_non_terminator].insert(i);
+			}
+		}
+		no_recursion.insert(non_terminator);
+	}
+	for (auto &new_non_terminator : new_non_terminators)
+	{
+		non_terminators.emplace_back(new_non_terminator);
+	}
+}
+
+void grammar::first_derivation(const token& non_terminator)//计算first集
+{
+	for (int i : productions_class[non_terminator])//遍历该非终结符对应的所有产生式
+	{
+		production current_production = productions[i];
+		bool all_epsilon = true;
+		for (const auto& j : current_production.right)//遍历该产生式右侧字符
+		{
+			if (find(terminators.begin(), terminators.end(), j) != terminators.end())//若为终结符则加入该产生式first集，否则继续遍历
+			{
+				all_epsilon = false;
+				first_of_production[i].insert(j);
+				break;
+			}
+			if (first_over[j] == false)//若为非终结符且为求取其first集，则递归求取
+			{
+				first_derivation(j);
+			}
+			first_of_production[i].insert(first[j].begin(), first[j].end());//若非终结符first集已计算则将其first集加入该产生式的first集
+			if (first[j].find("ε") == first[j].end())//若不含空产生式则结束遍历
+			{
+				all_epsilon = false;
+				break;
+			}
+			else//若含空产生式，删除first集中的ε，继续遍历
+			{
+				first_of_production[i].erase("ε");
+			}
+		}
+		if (all_epsilon)//若都为空，则将其first加入ε
+		{
+			first_of_production[i].insert("ε");
+		}
+		first[non_terminator].insert(first_of_production[i].begin(), first_of_production[i].end());//将该产生式first集加入所求非终结符的first集
+	}
+	first_over[non_terminator] = true;
+}
+
+void grammar::follow_derivation(const token& non_terminator) 
+{
+	for (const auto& i : non_terminators)//遍历非终结符
+	{
+		for (int j : productions_class[i])//遍历该非终结符对应的产生式
+		{
+			const auto& current_production = productions[j];
+			for (int t = 0,k;t < current_production.right.size();t++)//遍历产生式右侧所有字符
+			{
+				if (current_production.right[t] == non_terminator)//若遍历到该非终结符
+				{
+					for (k = t + 1;k < current_production.right.size();k++)//遍历该非终结符右侧所有字符
+					{
+						const auto& temp_token=current_production.right[k];
+						if (find(terminators.begin(), terminators.end(), temp_token) != terminators.end())//若为终结符则加入follow集
+						{
+							follow[non_terminator].insert(temp_token);
+							break;
+						}
+						follow[non_terminator].insert(first[temp_token].begin(), first[temp_token].end());//若为非终结符则将其first集加入该非终结符follow集中
+						if (first[temp_token].find("ε") == first[temp_token].end())//如无空产生式，终止遍历
+						{
+							break;
+						}
+						else//否则将ε从follow集中删除，继续遍历
+						{
+							follow[non_terminator].erase("ε");
+						}
+					}
+					if (k == current_production.right.size() && current_production.left != non_terminator)//如果该产生式遍历结束仍有空产生式
+					{
+						const auto& non_terminator2 = current_production.left;
+						follow_include[non_terminator][non_terminator2] = true;
+						if (!follow_include[non_terminator2][non_terminator])//若该产生式左侧不包含该非终结符
+						{
+							if (!(follow_over[non_terminator2]))
+							{
+								follow_derivation(non_terminator2);
+							}
+							follow[non_terminator].insert(follow[non_terminator2].begin(), follow[non_terminator2].end());//将该产生式左侧的follow集加入到该非终结符follow集中
+						}
+					}
+				}
+			}
+		}
+	}
+	follow_over[non_terminator] = true;
+}
+
+void grammar::output_productions()
+{
+	for (production i : productions)
+	{
+		cout << i.left << "=";
+		for(string j:i.right)
+			cout<<j;
+		cout << endl;
+	}
+}
+
+void grammar::output_first()
+{
+	for (const auto& i : productions)
+	{
+		first_of_production.push_back({});
+	}
+	for (token i : non_terminators)
+	{
+		first_derivation(i);
+	}
+}
+
+void grammar::output_follow()
+{
+	follow[start_token].insert("$");
+	for (token i : non_terminators)
+	{
+		if(!follow_over[i])
+			follow_derivation(i);
+	}
+	for (token i:non_terminators)
+	{
+		for (token j: non_terminators)
+		{
+			if (follow_include[i][j] && follow_include[j][i])
+			{
+				follow[i].insert(follow[j].begin(), follow[j].end());
+				follow[j] = follow[i];
+				follow_include[i][j] = follow_include[j][i] = false;
+			}
+		}
+	}	
+}
+
+void grammar::output_first_follow()
+{
+	cout << "----------------------------------------------" << endl;
+	cout << setiosflags(ios::left) << setw(15) << " " << setw(15) << "first" << setw(15) << "follow" << endl;
+	cout << "----------------------------------------------" << endl;
+	for (token i : non_terminators)
+	{
+		cout  << setw(15) << i + ": ";
+		string fi = "{",fo="{";
+		for (token j : first[i])
+		{
+			fi+=j+",";
+		}
+		fi=fi.substr(0, fi.size() - 1);
+		fi += "}";
+		for (token j : follow[i])
+		{
+			fo+=j+",";
+		}
+		fo=fo.substr(0, fo.size() - 1);
+		fo += "}";
+		cout << setw(15)<<fi<<setw(15)<<fo<<endl;
+	}
+}
+
+grammar::grammar()
+//:
+//	non_terminators({ "E", "T", "F" }),
+//	terminators(
+//		{
+//			"+", "-", "*", "/", "(", ")","n","ε","$"
+//		}
+//	),
+//	start_token("E"),
+//	productions( 
+//		{
+//			{
+//				"E", {"E","+","T"}
+//			},
+//			{
+//				"E", {"E","-","T"}
+//			},
+//			{
+//
+//				"E", {"T"}
+//
+//			},
+//			{
+//				"T", {"T","*","F"}
+//			},
+//			{
+//				"T", {"T","/","F"}
+//			},
+//			{
+//				"T", {"F"}
+//			},
+//			{
+//				"F", {"(","E",")"}
+//			},
+//			{
+//				"F", {"n"}
+//			}
+//		}
+//	)
+{
+	FILE *fptr1;
+	if(!(fptr1 = fopen("grammar.in", "r")))
+		printf("打开文件失败！");
+	char buf[100];
+	fgets(buf, 100, fptr1);
+	token s=buf;
+	s = s.substr(0, s.size() - 1);
+	non_terminators = split(s);
+	fgets(buf, 100, fptr1);
+	s = buf;
+	s = s.substr(0, s.size() - 1);
+	terminators = split(s);
+	terminators.push_back("ε");
+	terminators.push_back("$");
+	fgets(buf, 100, fptr1);
+	start_token = buf[0];
+	while (!feof(fptr1))
+	{
+		fgets(buf, 100, fptr1);
+		token left = string(1,buf[0]);
+		deque<token> right;
+		for (int i = 3;buf[i] != '\n'&&buf[i]!=-'\0';i++)
+		{
+			right.push_back(string(1, buf[i]));
+		}
+		production p = {left,right};
+		productions.push_back(p);
+	}
+	for (token i : non_terminators)
+	{
+		first_over.insert(pair<token, bool>(i, false));
+	}
+}
---
+++ LL(1)语法分析器-2018211393-王一丰/parser.cpp
@@ -0,0 +1,220 @@
+#include"parser.h"
+#include<iostream>
+#include<stack>
+#include <iomanip>
+void parser::input_token()
+{
+	FILE *fptr;
+	fptr = fopen("token_stream.in", "r");
+	string num="";
+	while (!feof(fptr))
+	{
+		char c = fgetc(fptr);
+		if (c == -1)
+		{
+			if (num.size())
+			{
+				token_stream.emplace_back("n");
+				num = "";
+			}
+			break;
+		}		
+		if (c > '9' || c < '0')
+		{
+			if (num.size())
+			{
+				token_stream.emplace_back("n");
+				num = "";
+			}
+			token t(1,c);
+			token_stream.emplace_back(t);
+		}
+		else
+		{
+			num += c;
+		}
+	}
+	token_stream.emplace_back("$");
+	
+}
+
+void parser::table_derivation()
+{
+	g.remove_recursion();
+	cout << "消除左递归后所有产生式：" << endl;
+	g.output_productions();
+	g.output_first();
+	g.output_follow();
+	cout <<endl<<"first_follow集：" << endl;
+	g.output_first_follow();
+	g.terminators.erase(find(g.terminators.begin(), g.terminators.end(), "ε"));
+	cout << endl << "预测分析表：" << endl;
+	for (token i : g.non_terminators)
+	{
+		for (int j : g.productions_class[i])
+		{
+			bool is_epsilon = false;
+			for (token t : g.first_of_production[j])
+			{
+				if (t != "ε")
+					parser_table[i][t] = j;
+				else
+					is_epsilon = true;
+			}
+			if (is_epsilon)
+			{
+				for (token t : g.follow[i])
+				{
+					parser_table[i][t] = j;
+				}
+			}
+		}
+		for (token t : g.follow[i])
+		{
+			if(parser_table[i].find(t)== parser_table[i].end())
+				parser_table[i][t] = -1;
+		}
+	}
+	for (token i : g.non_terminators)
+	{
+		for (token j : g.terminators)
+		{
+			if (parser_table[i].find(j) == parser_table[i].end())
+				parser_table[i][j] = -2;
+		}
+	}
+}
+
+void parser::output_parser_table()
+{
+
+	//for (token i : g.non_terminators)
+	//{
+	//	for (token j : g.terminators)
+	//	{
+	//		cout << parser_table[i][j] << "	";
+	//	}
+	//	cout << endl;
+	//}
+	cout << "-------------------------------------------------------------------------------------------" << endl;
+	cout << setiosflags(ios::left) << setw(10)<<" ";
+	for (token i : g.terminators)
+	{
+		cout << setw(10) <<i;
+	}
+	cout << endl;
+	cout << "-------------------------------------------------------------------------------------------" << endl;
+	for (token i : g.non_terminators)
+	{
+		cout << setw(10) <<i;
+		for (token j : g.terminators)
+		{
+			int temp = parser_table[i][j];
+			if (temp == -1)
+			{
+				cout <<setw(10) << "synch";
+				continue;
+			}
+			else if(temp==-2)
+			{
+				cout <<setw(10)<<"error";
+				continue;
+			}
+			token s= g.productions[temp].left+"->";
+			for (token t : g.productions[temp].right)
+			{
+				s = s+t;
+			}
+			cout << setw(10)<<s;
+			
+		}
+		cout << endl;
+	}
+}
+
+void parser::predict()
+{
+	cout << endl << "预测过程：" << endl;
+	cout << "-----------------------------------------------" << endl;
+	cout << setiosflags(ios::left) << setw(10)<<"序号"<<setw(30) << "左句型"
+		<< setw(20) << "输入流" << setw(20)<<"输出" << endl;
+	cout << "-----------------------------------------------" << endl;
+	stack<token> parser_stack;
+	pair<deque<token>, deque<token>> left_pattern{ {},{g.start_token} };
+	parser_stack.push("$");
+	parser_stack.push(g.start_token);
+	vector<token>::iterator iter = token_stream.begin();
+	for (int d=1;iter != token_stream.end();d++)
+	{
+		if (parser_stack.empty())
+			break;
+		cout << setw(10) << d;
+		string s = "";
+		for (token t : left_pattern.first)
+		{
+			s+=t;
+		}
+		for (token t : left_pattern.second)
+		{
+			s+= t;
+		}
+		cout << setw(30) << s;
+		vector<token>::iterator iter2 = iter;
+		s = "";
+		for (;iter2 != token_stream.end();iter2++)
+		{
+			s+=*iter2;
+		}
+		cout << setw(20)<<s;
+		s = "";
+		if (find(g.terminators.begin(), g.terminators.end(), parser_stack.top()) != g.terminators.end())//栈顶为非终结符
+		{
+			if (*iter == parser_stack.top())
+			{
+				s += "匹配成功：" + *iter;
+				iter++;			
+			}
+			else
+			{
+				s+="error:" + parser_stack.top() + " expected";
+			}
+			parser_stack.pop();
+			if (left_pattern.second.size())
+			{
+				left_pattern.first.push_back(left_pattern.second.front());
+				left_pattern.second.pop_front();
+			}
+		}
+		else//栈顶为终结符
+		{
+			int p = parser_table[parser_stack.top()][*iter];
+			if (p == -2)
+			{
+				s+= "error:" + *iter + " unexpected";
+				iter++;
+			}
+			else if (p == -1)
+			{
+				s+="error:"+parser_stack.top()+"expected";
+				parser_stack.pop();
+			}
+			else
+			{
+				parser_stack.pop();
+				left_pattern.second.pop_front();
+				s+=g.productions[p].left+"->";
+				for (string j : g.productions[p].right)
+					s+=j;
+				if (g.productions[p].right.front()!= "ε")
+				{
+					for (int i = g.productions[p].right.size() - 1;i >= 0;i--)
+					{
+						parser_stack.push(g.productions[p].right[i]);
+					}
+					left_pattern.second.insert(left_pattern.second.begin(), g.productions[p].right.begin(), g.productions[p].right.end());
+				}				
+			}
+		}
+		cout <<setw(20)<< s << endl;
+	}
+}
